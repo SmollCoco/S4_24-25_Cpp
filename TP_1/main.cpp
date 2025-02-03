6884d43b842bc912aa38df98254b891468c49d87
SmollCoco <144068020+SmollCoco@users.noreply.github.com>
2025-02-03 22:21:09 +0100

    Add files via upload

---
+++ TP_1/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#ifndef EXO
+#define EXO 1
+#endif
+
+#if (EXO == 1)
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Bienvenue en C++!" << std::endl;
+    return 0;
+}
+
+#elif (EXO == 2)
+int main() {
+    std::cout << "Entrez un nombre: ";
+    int x;
+    std::cin >> x;
+    std::cout << "Vous avez entré: " << x << std::endl;
+    return 0;
+}
+
+#elif (EXO == 3)
+int main() {
+    std::cout << "Entrez deux nombres: ";
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << "Somme: " << x + y << std::endl;
+    std::cout << "Différence: " << x - y << std::endl;
+    std::cout << "Produit: " << x * y << std::endl;
+    std::cout << "Quotient: " << x / y << std::endl;
+    return 0;
+}
+    
+#elif (EXO == 4)
+int main() {
+    int nombre;
+    std::cin >> nombre;
+    std::cout << "Vous avez entré: " << nombre << std::endl;
+    return 0;
+}
+
+#elif (EXO == 5)
+int main() {
+    std::cout << "Entrez une température en Celsius: ";
+    int c;
+    std::cin >> c;
+    std::cout << "En Fahrenheit: " << (c * 9.0 / 5 + 32) << "°F" << std::endl;
+    return 0;
+}
+
+#endif
